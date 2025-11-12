57c024744046fe928c9bf9eea938f50bed355c6f
mjsampaio <mjsampaio@gmail.com>
2025-11-12 20:12:42 +0000

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('vr-layout-v1').then(c => c.addAll(['./','./index.html','./style.css','./script.js','./data/vr_bus.json'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
